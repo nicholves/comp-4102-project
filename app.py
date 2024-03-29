18dff553d242ffdb708e7dd04b80a6e2b3e9c4c2
Paul Airuehia <osasuair@gmail.com>
2024-03-29 15:36:58 -0400

    Added dummy client and Added NutritionLabel Class

--- app.py
+++ app.py
@@ -1,34 +1,46 @@
+import json
 from flask import Flask, request, redirect
+from flask_cors import CORS
 from skimage import io
-import os
 
 import imageProcess
 
-UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+CORS(app)
 app.config['MAX_CONTENT_LENGTH'] = 25 * 1000 * 1000
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-    
+
+# POST request to upload image
 @app.route('/', methods=['POST'])
 def upload():
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        return "no file part", 400
-    file = request.files['file']
     
-    # If the user does not select a file, the browser submits an
+    # check if the post request has the image
+    if 'image' not in request.files:
+        return "Image with name 'image' not included", 400
+    file = request.files['image']
+    
+    # If the client does not select a file, the browser submits an
     # empty file without a filename.
-    if file.filename == '':
-        return "no selected file", 400
+    if file and file.filename == '' or not allowed_file(file.filename):
+        return "file is invalid", 400    
+
+    # process the image
+    image = io.imread(file)
+    nutrition_label, success = imageProcess.processImage(image)
     
-    if file and allowed_file(file.filename):
-        image = io.imread(file)
-        np_img = imageProcess.processImage(image)
-        
-        return str(np_img), 200
+    if not success:
+        return "Nutrition Label Invalid", 400
+    
+    # set response content type to json
+    response = app.response_class(
+        response=json.dumps(nutrition_label.toDict()),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
--- imageProcess.py
+++ imageProcess.py
@@ -3,11 +3,68 @@ import matplotlib.pyplot as plt # plotting
 import matplotlib.image as mpimg # reading images
 from skimage.color import rgb2gray # converting rgb images to grayscale
 
+class SpecialNutrients:
+    def __init__(self, vitamin_d, calcium, iron, potassium):
+        self.vitamin_d = vitamin_d
+        self.calcium = calcium
+        self.iron = iron
+        self.potassium = potassium
+
+    def __str__(self):
+        return f"Special Nutrients:\nVitamin D: {self.vitamin_d}\nCalcium: {self.calcium}\nIron: {self.iron}\nPotassium: {self.potassium}"
+
+class NutritionLabel ():   
+    def __init__(self, servings_per_container, serving_size, calories, total_fat, saturated_fat, trans_fat, cholesterol, sodium, total_sugars, total_carbs, dietary_fiber, protein, vitamin_d, calcium, iron, potassium):
+        self.servings_per_container = servings_per_container
+        self.serving_size = serving_size
+        self.calories = calories
+        self.total_fat = total_fat
+        self.saturated_fat = saturated_fat
+        self.trans_fat = trans_fat
+        self.cholesterol = cholesterol
+        self.sodium = sodium
+        self.total_carbs = total_carbs
+        self.dietary_fiber = dietary_fiber
+        self.total_sugars = total_sugars
+        self.protein = protein
+        self.special_nutrients = SpecialNutrients(vitamin_d, calcium, iron, potassium)
+    
+    def toDict(self):
+        return {
+            "servings_per_container": self.servings_per_container,
+            "serving_size": self.serving_size,
+            "calories": self.calories,
+            "total_fat": self.total_fat,
+            "saturated_fat": self.saturated_fat,
+            "trans_fat": self.trans_fat,
+            "cholesterol": self.cholesterol,
+            "sodium": self.sodium,
+            "total_carbs": self.total_carbs,
+            "dietary_fiber": self.dietary_fiber,
+            "total_sugars": self.total_sugars,
+            "protein": self.protein,
+            "special_nutrients": {
+                "vitamin_d": self.special_nutrients.vitamin_d,
+                "calcium": self.special_nutrients.calcium,
+                "iron": self.special_nutrients.iron,
+                "potassium": self.special_nutrients.potassium
+            }
+        }
+
+
 def processImage(image):
     # convert to grayscale
     image = rgb2gray(image)
     
-    # print image to console
-    print(image)   
+    # TODO: Do image preprocessing here
+    
+    # TODO: Find nutrition label in image with ml model
+    
+    # TODO: Use OCR to extract text from nutrition label
+    
+    # TODO: Parse text to get nutrition label values
+    
+    # Temporary code to test Nutrition Label JSON object
+    nutrition_label = NutritionLabel("1", "1 cup", "100", "10g", "5g", "0g", "0mg", "0mg", "10g", "20g", "5g", "5g", "1000IU", "1000mg", "10mg", "100mg")
 
-    return image
+    return nutrition_label, True
